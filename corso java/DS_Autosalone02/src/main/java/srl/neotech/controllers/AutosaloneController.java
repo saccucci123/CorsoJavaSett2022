83fdb5d17e53bbfde5839065ff850c6a66b1b9fe
daniele <daniele.saccucci@live.it>
2022-12-29 12:39:25 +0100

    primo parte autosalone pt1

---
+++ corso java/DS_Autosalone02/src/main/java/srl/neotech/controllers/AutosaloneController.java
@@ -0,0 +1,41 @@
+package srl.neotech.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import srl.neotech.model.Automobile;
+import srl.neotech.model.Autosalone;
+
+@Controller
+public class AutosaloneController {
+
+	@Autowired
+	Autosalone autoSalone;
+	
+	@GetMapping("/listAuto")
+	public String getListaAuto(ModelMap modelMap) {
+		
+		modelMap.addAttribute("listaAuto",autoSalone.getAutomobili());
+		return "listaAuto";
+	}
+	
+	
+	@GetMapping("/addAuto")
+    public String aggiungiAuto(ModelMap modelMap) {
+        modelMap.addAttribute("automobili", autoSalone.getAutomobili());
+        return "addAuto";
+
+    }
+
+    @PostMapping("/add")
+    public String Add(@ModelAttribute  Automobile auto, ModelMap modelMap) {
+
+        autoSalone.getAutomobili().add(auto);
+        return "redirect:/listAuto";
+    }
+}
